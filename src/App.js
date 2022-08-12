89deea6b980e8e48e2f0d42cb909af248c367fbe
Rakshit <rakshit087@gmail.com>
2022-08-12 22:56:26 +0530

    Added Rainbow and WAGMI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 const App = () => {
   return (
     <div className="App">
+      <ConnectButton />
       <p>Hello World</p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Kue3",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ChakraProvider>
   </React.StrictMode>
 );
