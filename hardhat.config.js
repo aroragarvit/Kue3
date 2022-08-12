dfe3420493047294d6611f8f629c39c1b93f0ee1
Rakshit <rakshit087@gmail.com>
2022-08-12 21:02:18 +0530

    Added Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  paths: {
+    sources: "./src/hardhat/contracts",
+    artifacts: "./src/hardhat/artifacts",
+    tests: "./src/hardhat/tests",
+  },
 };
