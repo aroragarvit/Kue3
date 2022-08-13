da2c084bcf1d62fc72861fdb2dfc4dd0ca71273f
Rakshit <rakshit087@gmail.com>
2022-08-13 21:58:07 +0530

    Added Navbar and Dark Theme

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Navbar } from "./layouts/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <Navbar />
       <App />
     </ChakraProvider>
   </React.StrictMode>
