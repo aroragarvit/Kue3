6f3cd09ef2657d1ce84bdee547114f6b6417e9be
Rakshit <rakshit087@gmail.com>
2022-08-17 01:01:48 +0530

    Renamed App.js to home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,29 @@
-import { Question } from "./components/Question";
 import { Box } from "@chakra-ui/react";
+import { QuestionCard } from "../components/QuestionCard";
 import { useContractRead } from "wagmi";
-import kueContract from "./hardhat/artifacts/src/hardhat/contracts/kueContract.sol/Kue.json";
-const abi = kueContract.abi;
-const App = () => {
+import { useAbi } from "../hooks/useAbi";
+
+const Home = () => {
+
+  const abi = useAbi();
   const { data, isLoading } = useContractRead({
     addressOrName: process.env.REACT_APP_CONTRACT_ADDRESS,
     contractInterface: abi,
     functionName: "getLatestQuestion",
     args: [1],
   });
-  // add page
+
   return (
     <Box py={32} w={"full"} px={16}>
       {isLoading ? (
         <Box>Loading...</Box>
       ) : data ? (
         data.map((eachQue) => {
-          return <Question key={eachQue.id} question={eachQue} />;
+          return <QuestionCard key={eachQue.id} question={eachQue} />;
         })
       ) : null}
     </Box>
   );
 };
 
-export default App;
+export default Home;
