f16cd92ee7a95cccfdeb2f3d78bb5f5b2d5a01f2
garvit <agarvit1142000@gmail.com>
2022-08-17 02:18:58 +0530

    Go to question link by id on clicking on its title

--- src/pages/questionDesc.js
+++ src/pages/questionDesc.js
@@ -1,10 +1,15 @@
-import { Box, Button, Text, Flex, useColorModeValue } from "@chakra-ui/react";
-
-// basic react template
-export default function QuestionDesc() {
-  return (
-    <Box>
-      <Text>Answer</Text>
-    </Box>
-  );
+import { useContractRead } from "wagmi";
+import { useAbi } from "../hooks/useAbi";
+import { Box } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+export default function QuestionCard() {
+  const { id } = useParams();
+  const abi = useAbi();
+  const { data, isLoading } = useContractRead({
+    addressOrName: process.env.REACT_APP_CONTRACT_ADDRESS,
+    contractInterface: abi,
+    functionName: "getQuestionById",
+    args: [id],
+  });
+  console.log(data);
 }
