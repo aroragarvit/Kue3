7c983345a69ee5430d3efe6e08672fd584867237
Rakshit <rakshit087@gmail.com>
2022-08-19 02:04:49 +0530

    Worked on Answers Box

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,8 +1,8 @@
 import { useContractRead } from "wagmi";
 import { useAbi } from "../hooks/useAbi";
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue, Text } from "@chakra-ui/react";
 import AnswerCard from "../components/AnswerCard";
-export default function Answers(id) {
+export default function Answers({ id }) {
   const abi = useAbi();
   const { data, isLoading } = useContractRead({
     addressOrName: process.env.REACT_APP_CONTRACT_ADDRESS,
@@ -12,18 +12,30 @@ export default function Answers(id) {
   });
   console.log(data);
   return (
-    <Box mb={8} py={32} width={"50%"} marginX={"auto"}>
-      {isLoading ? (
-        <Box>Loading...</Box>
-      ) : data ? (
-        data.map((eachAns) => {
-          return (
-            <Box key={eachAns.id.toString()} mb={8} maxW={"60%"} marginX="auto">
-              <AnswerCard key={eachAns.id.toString()} />
+    <Box
+      my={8}
+      p={4}
+      borderWidth={1}
+      borderRadius="xl"
+      borderColor={useColorModeValue("gray.300", "gray.600")}
+    >
+      <Text fontSize={"3xl"}> Answers </Text>
+      <hr />
+      <Box py={4}>
+        {isLoading && <Box>Loading...</Box>}
+        {!isLoading &&
+          data &&
+          data.map((answer) => (
+            <Box key={answer.toString()}>
+              <AnswerCard answer={answer.toString()} />
             </Box>
-          );
-        })
-      ) : null}
+          ))}
+        {!isLoading && !data && (
+          <Text textAlign={"center"}>
+            No answers for this question has been submitted yet.
+          </Text>
+        )}
+      </Box>
     </Box>
   );
 }
