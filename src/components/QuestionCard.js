8c8e3b79e511e523ca76f0934b10a72bca495561
Rakshit <rakshit087@gmail.com>
2022-08-21 20:37:16 +0530

    Migrated homepage & QuestionCard

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,44 +1,30 @@
-import {
-  Box,
-  Button,
-  Text,
-  Flex,
-  useColorModeValue,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Box, Button, Text, Flex, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 import { AnswerModal } from "./AnswerModal";
-import { Link } from "react-router-dom";
+import { useRouter } from "next/router";
 
 export const QuestionCard = ({ question }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
   return (
-    <Box
-      border={"1px solid"}
-      borderRadius="xl"
-      p={4}
-      borderColor={useColorModeValue("gray.300", "gray.600")}
-    >
+    <Box border={"1px solid"} borderRadius="xl" p={4} borderColor={useColorModeValue("gray.300", "gray.600")}>
       <Flex justifyContent={"space-between"} alignContent="center">
-        <Link to={`/questionDesc/${question.id.toString()}`}>
-          <Text fontSize={"xl"} as="strong" lineHeight={2}>
-            Blockchain
-          </Text>
-        </Link>
+        <Text
+          fontSize={"xl"}
+          as="strong"
+          lineHeight={2}
+          onClick={() => {
+            router.replace(`/questionDesc/${question.id.toString()}`);
+          }}
+        >
+          Blockchain
+        </Text>
         <Button isDisabled colorScheme="blue" borderRadius={999} h={6}>
-          <Text fontSize={"xs"}>
-            {(parseInt(question.poolMoney) / 10e17).toString()} MATIC
-          </Text>
+          <Text fontSize={"xs"}>{(parseInt(question.poolMoney) / 10e17).toString()} MATIC</Text>
         </Button>
       </Flex>
       <Text>{question.question}</Text>
 
-      <Button
-        onClick={onOpen}
-        type="solid"
-        colorScheme="blue"
-        marginTop={8}
-        size="sm"
-      >
+      <Button onClick={onOpen} type="solid" colorScheme="blue" marginTop={8} size="sm">
         Answer
       </Button>
       <AnswerModal
