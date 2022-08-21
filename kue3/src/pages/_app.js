f4cc72990da4b14cc96e72fbb3372682dc55a2a0
Rakshit <rakshit087@gmail.com>
2022-08-21 19:56:58 +0530

    Added Wagmi, Chakra UI, and Rainbow kit

--- kue3/src/pages/_app.js
+++ kue3/src/pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
+import { Navbar } from "../layouts/Navbar";
+import { Sidebar } from "../layouts/Sidebar";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains([chain.hardhat, chain.polygonMumbai], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Kue3",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  persister: null,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Navbar />
+          <Flex flexDirection={"row-reverse"}>
+            {/* <Sidebar /> */}
+            <Component {...pageProps} />
+          </Flex>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
