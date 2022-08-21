74af44cd7550214df3e2b065c145568ad6a260d9
Rakshit <rakshit087@gmail.com>
2022-08-21 19:55:52 +0530

    Migrated NavBar

--- kue3/src/layouts/Navbar.js
+++ kue3/src/layouts/Navbar.js
@@ -2,10 +2,12 @@ import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
 import { useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Link } from "react-router-dom";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const router = useRouter();
+
   return (
     <Box
       bg={useColorModeValue("gray.100", "gray.900")}
@@ -18,7 +20,14 @@ export const Navbar = () => {
       zIndex={1}
     >
       <Flex h="9.8vh" alignItems={"center"} justifyContent={"space-between"}>
-        <Link to={"/"}><Text fontSize="3xl">Kue3</Text></Link>
+        <Text
+          fontSize="3xl"
+          onClick={() => {
+            router.replace("/");
+          }}
+        >
+          Kue3
+        </Text>
         <Flex>
           <IconButton
             aria-label="theme switcher"
