e6e348591230568d37e4d38ef7518c66a7ddecc9
Rakshit <rakshit087@gmail.com>
2023-11-27 23:21:35 +0530

    updated write questions

--- src/components/AddQuestionButton.js
+++ src/components/AddQuestionButton.js
@@ -2,64 +2,66 @@ import { AddIcon } from "@chakra-ui/icons";
 import { Button, useToast } from "@chakra-ui/react";
 import { ethers } from "ethers";
 import { useAbi } from "../hooks/useAbi";
-import { useEffect, useState } from "react";
-import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+import { prepareWriteContract, writeContract, waitForTransaction } from "@wagmi/core";
 import axios from "axios";
 
 export const AddQuestionButton = ({ value, title, question }) => {
   const toast = useToast();
   const abi = useAbi();
-  const [cid, setCid] = useState();
-  const { config } = usePrepareContractWrite({
-    addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-    contractInterface: abi,
-    functionName: "createQuestion",
-    args: [title, cid],
-    overrides: {
-      value: ethers.utils.parseEther(value),
-    },
-  });
-  const { data, write } = useContractWrite(config); // write is a function that writes to the contract
-  const { isSuccess } = useWaitForTransaction({ hash: data?.hash });
-  useEffect(() => {
-    if (isSuccess) {
+
+  const handleClick = async () => {
+    const cid = await postToIPFS();
+    const config = await createConfig(cid);
+    const { hash } = await writeContract(config);
+    toast({
+      title: "Success",
+      description: "Your question has been posted!",
+      status: "success",
+      isClosable: true,
+    });
+  };
+  const postToIPFS = async () => {
+    try {
+      const res = await axios.post("/api/addtext", {
+        text: question,
+      });
+      return res.data.cid;
+    } catch (error) {
       toast({
-        title: "Success",
-        description: "Question added successfully",
-        status: "success",
+        title: "Error",
+        description: error.message,
+        status: "error",
         isClosable: true,
       });
-      setTimeout(function () {
-        window.location.href = "/";
-      }, 1000);
+      console.log(error);
     }
-  }, [isSuccess]);
+  };
+
+  const createConfig = async (cid) => {
+    try {
+      const config = await prepareWriteContract({
+        address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+        abi: abi,
+        functionName: "createQuestion",
+        args: [title, cid],
+        overrides: {
+          value: ethers.utils.parseEther(value),
+        },
+      });
+      return config;
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        isClosable: true,
+      });
+      console.log(error);
+    }
+  };
+
   return (
-    <Button
-      colorScheme="blue"
-      variant="solid"
-      leftIcon={<AddIcon />}
-      onClick={async () => {
-        try {
-          const res = await axios.post("/api/addtext", {
-            text: question,
-          });
-          console.log(res.data.cid);
-          setCid(res.data.cid);
-          setTimeout(() => {
-            write();
-          }, 1000);
-        } catch (error) {
-          toast({
-            title: "Error",
-            description: error.message,
-            status: "error",
-            isClosable: true,
-          });
-          console.log(error);
-        }
-      }}
-    >
+    <Button colorScheme="blue" variant="solid" leftIcon={<AddIcon />} onClick={handleClick}>
       Post Question
     </Button>
   );
