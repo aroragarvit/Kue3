2cab51a040f9c269a7640eba4a5b5bb2ba66ea0d
Rakshit <rakshit087@gmail.com>
2023-11-29 23:59:18 +0530

    updated approve

--- src/components/Approve.js
+++ src/components/Approve.js
@@ -1,53 +1,46 @@
-import {
-  useContractWrite,
-  usePrepareContractWrite,
-  useWaitForTransaction,
-} from "wagmi";
 import { useAbi } from "../hooks/useAbi";
-import { useEffect } from "react";
-import {
-  Button,
-  Flex,
-  useToast,
-} from "@chakra-ui/react";
+import { Button, Flex, useToast } from "@chakra-ui/react";
+import { prepareWriteContract, writeContract, waitForTransaction } from "@wagmi/core";
 
-export default function Approve({
-  answerId = answerId,
-  questionId = questionId,
-}) {
+export default function Approve({ answerId = answerId, questionId = questionId }) {
   const abi = useAbi();
   const toast = useToast();
-  const { config } = usePrepareContractWrite({
-    addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-    contractInterface: abi,
-    functionName: "approvePayment",
-    args: [questionId, answerId],
-  });
-  const { data, write } = useContractWrite(config); // write is a function that writes to the contract
-  const { isSuccess } = useWaitForTransaction({ hash: data?.hash });
-  useEffect(() => {
-    if (isSuccess) {
-      toast({
-        title: "Success",
-        description: "Payment Approved",
-        status: "success",
 
+  const handleClick = async () => {
+    const config = await createConfig(answer);
+    await writeContract(config);
+    toast({
+      title: "Success",
+      description: "Your question has been posted!",
+      status: "success",
+      isClosable: true,
+    });
+  };
+
+  const createConfig = async () => {
+    try {
+      const config = await prepareWriteContract({
+        address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+        abi: abi,
+        functionName: "approvePayment",
+        args: [questionId, answerId],
+      });
+      return config;
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
         isClosable: true,
       });
+      console.log(error);
     }
-  }, [isSuccess]);
+  };
+
   return (
     <Flex justifyContent={"right"} w={"full"}>
-      <Button
-        colorScheme="blue"
-        variant="solid"
-        size={"sm"}
-        mt={4}
-        onClick={() => {
-          write();
-        }}
-      >
-        Approve 
+      <Button colorScheme="blue" variant="solid" size={"sm"} mt={4} onClick={handleClick}>
+        Approve
       </Button>
     </Flex>
   );
